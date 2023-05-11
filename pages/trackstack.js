77491fbec99d91b819367159ae87177922a8c896
Jay Hill <jayhill@Jays-MacBook-Pro.local>
2023-05-11 10:49:00 -0500

    trackstack label boxes

---
+++ pages/trackstack.js
@@ -0,0 +1,24 @@
+import trackstack from '../public/trackstacklabels.json';
+import Link from 'next/link';
+
+const labels = trackstack.data.searchInboxes.map( label => {
+    const slug = label.slug
+    return (
+        slug
+    );
+});
+console.log ( labels );
+export default function Trackstack() {
+    return (
+        <>
+            <div>
+            {labels.map( label => (
+                <div className="grid-flow-column grid">
+                    <Link key={label} className="text-black" href={`https://trackstack.app/inbox/${label}`}>{label}</Link>      
+                    </div>
+            ))}
+        </div>
+        </>
+
+    )
+}
